c45684a547542d2492ff5071805c24aa184f386d
rod409 <radoyeh@gmail.com>
2025-04-21 19:02:41 -0700

    Use ijson to parse segmentation accuracy log for memory efficiency

--- automotive/semantic-segmentation/accuracy_cognata.py
+++ automotive/semantic-segmentation/accuracy_cognata.py
@@ -4,12 +4,11 @@ We assume that loadgen's query index is in the same order as
 the images in coco's annotations/instances_val2017.json.
 """
 import argparse
-import json
+import ijson
 import os
 import numpy as np
-from cognata import Cognata, prepare_cognata, train_val_split
+from cognata import Cognata
 import torch
-import cognata_scenarios
 from utils import StreamSegMetrics, read_dataset_csv
 from utils import ext_transforms as et
 # pylint: disable=missing-docstring
@@ -53,9 +52,6 @@ def get_args():
 def main():
     args = get_args()
 
-    with open(args.mlperf_accuracy_file, "r") as f:
-        results = json.load(f)
-
     seen = set()
     num_classes = 19
     metrics = StreamSegMetrics(num_classes)
@@ -71,27 +67,24 @@ def main():
                         std=[0.229, 0.224, 0.225]),
     ])
     val_loader = Cognata(files, transform=val_transform)
-    for j in results:
-        idx = j['qsl_idx']
-        # de-dupe in case loadgen sends the same image multiple times
-        if idx in seen:
-            continue
-        seen.add(idx)
-        _, target = val_loader.get_item(idx)
-        # reconstruct from mlperf accuracy log
-        # what is written by the benchmark is an array of float32's:
-        # id, box[0], box[1], box[2], box[3], score, detection_class
-        # note that id is a index into instances_val2017.json, not the actual
-        # image_id
-        prediction = np.frombuffer(
-            bytes.fromhex(
-                j['data']),
-            np.uint8).astype(int)
-        metrics.update(
-            target.cpu().to(
-                dtype=torch.int32).numpy().reshape(
-                1, image_size[0], image_size[1]), prediction.reshape(
-                1, image_size[0], image_size[1]))
+    with open(args.mlperf_accuracy_file, "r") as f:
+        for j in ijson.items(f, 'item'):
+            idx = j['qsl_idx']
+            # de-dupe in case loadgen sends the same image multiple times
+            if idx in seen:
+                continue
+            seen.add(idx)
+            _, target = val_loader.get_item(idx)
+
+            prediction = np.frombuffer(
+                bytes.fromhex(
+                    j['data']),
+                np.uint8)
+            metrics.update(
+                target.cpu().to(
+                    dtype=torch.uint8).numpy().reshape(
+                    1, image_size[0], image_size[1]), prediction.reshape(
+                    1, image_size[0], image_size[1]))
 
     score = metrics.get_results()
     print(metrics.to_str(score))
