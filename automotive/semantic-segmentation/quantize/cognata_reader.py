8cbb7b5eb92b09ce5de2e447004fb8f10bb39fd6
rod409 <radoyeh@gmail.com>
2025-05-01 18:51:06 -0700

    Add quantization for deeplabv3+

---
+++ automotive/semantic-segmentation/quantize/cognata_reader.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+import os
+import pickle
+from onnxruntime.quantization import CalibrationDataReader
+import pickle
+
+def to_numpy(tensor):
+    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+class Cognata(CalibrationDataReader):
+    def __init__(self, data_root, num_items):
+        self.data_root = data_root
+        self.length = num_items
+        self.data = []
+        for i in range(num_items):
+            file_path = os.path.join(self.data_root, f'calib_{i}.pkl')
+            with open(file_path, 'rb') as f:
+                item = pickle.load(f)
+                
+                self.data.append({'input_image': to_numpy(item['img'].unsqueeze(0))})
+        self.enum_data = iter(self.data)
+
+    def __len__(self):
+        return len(self.data)
+    
+    def get_next(self):
+        if self.enum_data is None:
+            self.enum_data = iter(self.data)
+        return next(self.enum_data, None)
+    
+    def rewind(self):
+        self.enum_data = iter(self.data)
---
+++ automotive/semantic-segmentation/quantize/quantize.py
@@ -0,0 +1,31 @@
+import onnx
+import onnxruntime.quantization as quantization
+import argparse
+import cognata_reader
+import csv
+
+def read_dataset_csv(file_path):
+    files = []
+    with open(file_path, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            files.append(row)
+    return files
+
+def main():
+    parser = argparse.ArgumentParser(description="Quantize an ONNX model.")
+    parser.add_argument("model_fp32", type=str, help="Path to the input ONNX model.")
+    parser.add_argument("model_prep_path", type=str, help="Path to the prepped ONNX model.")
+    parser.add_argument("model_quant", type=str, help="Path to save the quantized ONNX model.")
+    parser.add_argument("--data-root", type=str, help="path to calibration files")
+    args = parser.parse_args()
+    files = read_dataset_csv("calibration_set.csv")
+    cal_reader = cognata_reader.Cognata(args.data_root, len(files))
+    #quantized_model = quantize_dynamic(args.model_fp32, args.model_quant, weight_type=QuantType.Qfp)
+    quantization.shape_inference.quant_pre_process(args.model_fp32, args.model_prep_path)
+    quantized_model = quantization.quantize_static(args.model_prep_path, args.model_quant, cal_reader)
+    
+    print(f"Quantized model saved to {args.model_quant}")
+
+if __name__ == "__main__":
+    main()
