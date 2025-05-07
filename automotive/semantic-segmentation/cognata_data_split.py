bdaa046a975f0694a0778b2e3232c0d8e435e9a0
rod409 <radoyeh@gmail.com>
2025-05-07 12:52:11 -0700

    Enable setting validation set size during cognata data split

--- automotive/semantic-segmentation/cognata_data_split.py
+++ automotive/semantic-segmentation/cognata_data_split.py
@@ -1,6 +1,6 @@
 import argparse
 import importlib
-from cognata import Cognata, prepare_cognata, train_val_split
+from utils import cognata_raw
 import cognata_scenarios
 import csv
 
@@ -17,6 +17,12 @@ def get_args():
         type=int,
         help="number of items in calibration dataset",
     )
+    parser.add_argument(
+        "--validation-size",
+        default=6000,
+        type=int,
+        help="number of items in validation dataset",
+    )
     args = parser.parse_args()
     return args
 
@@ -31,14 +37,14 @@ def write_data_to_csv(file_path, files):
 
 def main():
     args = get_args()
-    files = prepare_cognata(
+    files = cognata_raw.prepare_cognata(
         args.dataset_path, cognata_scenarios.folders, cognata_scenarios.cameras)
-    files = train_val_split(files, args.calibration_size)
+    files = cognata_raw.train_val_split(files, args.calibration_size)
     for file_set in files.values():
         for file in file_set:
             file['img'] = file['img'].replace(args.dataset_path, '')
             file['label'] = file['label'].replace(args.dataset_path, '')
-    write_data_to_csv('val_set.csv', files['val'])
+    write_data_to_csv('val_set.csv', files['val'][:args.validation_size])
     write_data_to_csv('calibration_set.csv', files['calibration'])
 
 
